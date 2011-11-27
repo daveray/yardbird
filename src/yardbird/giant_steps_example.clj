362c2453ba38d467918ebcd094618671518ccfe4
Dave Ray <daveray@gmail.com>
2011-11-27 13:05:23 -0500

    commentary

--- src/yardbird/giant_steps_example.clj
+++ src/yardbird/giant_steps_example.clj
@@ -16,33 +16,42 @@
 
 (boot-external-server)
 
+; Make a bass-like instrument (aka beep)
 (definst my-bass [note 60 vol 0.35]
   (let [freq (midicps note)
         src (sin-osc freq)
         env (env-gen (perc ) :action FREE)]
     (* vol src env)))
 
+; Use MdaPiano ugen
 (defn my-piano 
   ([note] (my-piano note 50))
   ([note vel] (piano note 1 (+ vel (rand-int 10)) 0.3 0.1 0.5)))
 
+; Grab all the notes in the original Giant Steps solo
 (def gs1 (mapcat #(map note %) (load-solo :gs1)))
+
+; Roots of the chord changes, 2 beats each
 (def gs1-changes 
   [:B4  :D4  :G4  :Bb4 :Eb4 :Eb4 :A4  :D4  
    :G4  :Bb4 :Eb4 :F#4 :B4  :B4  :F4  :Bb4 
    :Eb4 :Eb4 :A4  :D4  :G4  :G4  :C#4 :F#4 
    :B4  :B4  :F4  :Bb4 :Eb4 :Eb4 :C#4 :F#4])
 
+; Build an infinite, dumb 1-5 bass line
 (def gs1-bass-line
   (let [roots (->> gs1-changes
                 (map note)
-                (map (absolute-transpose -12)))
-        fifths (map (absolute-transpose 7) roots)
-        line   (interleave roots fifths)]
+                (map (absolute-transpose -12)))   ; bassier
+        fifths (map (absolute-transpose 7) roots) ; calculate fifths
+        line   (interleave roots fifths)]         ; mix roots and fifths
     (cycle (stretch 2 line)))) ; stretch to match solo eighth notes
 
 ((note-player :dt 100) 
+   ; Play the solo with the piano, harmonized in absolute fifths
    { :inst my-piano :notes (map (juxt identity (absolute-transpose 7)) gs1)}
+
+   ; Play the bass-line
    { :inst my-bass  :notes gs1-bass-line })
 
 (stop)
