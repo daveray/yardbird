b50fc369c5215d1cc44e60d852fd4c0924c17418
Dave Ray <daveray@gmail.com>
2011-11-28 22:07:42 -0500

    clojars and version

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject yardbird "0.0.0-SNAPSHOT"
+(defproject yardbird "0.0.1"
   :description "Overtone playground"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [overtone "0.5.0"]])
+                 [overtone "0.5.0"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
