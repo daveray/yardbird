a436e07861c645543323d7301f55b0e2db454327
Dave Ray <daveray@gmail.com>
2012-01-06 20:39:07 -0500

    use overtone 0.6

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject yardbird "0.0.2-SNAPSHOT"
   :description "Overtone playground"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [overtone "0.5.0"]]
+                 [overtone "0.6.0"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
